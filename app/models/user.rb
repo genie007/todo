f666260f079be533816404de2689e8b74acfe4e7
unknown <pmallaya@wks-pmallaya.elogex.com>
2012-01-05 15:39:30 -0600

    make sure authentication token is generated before a user is saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
 		 :token_authenticatable
-
+  before_save :ensure_authentication_token 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 end
